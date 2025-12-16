4bdde62b4cd9f326a293eaad8c4b06ef7f9ee250
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2025-12-16 23:15:36 +0100

    established main sectin

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,23 @@ import AppSideBar from './components/AppSideBar.vue';
 <template>
   <div class="app">
     <AppSideBar/>
+    <div class="main">
+      <RouteView/>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.app {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+
+  .main {
+    width: calc(100vw - 40px);
+    height: 100vh;
+    padding: 0;
+    margin: 0;
+  }
+}
 </style>
