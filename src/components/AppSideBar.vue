b5851a91756098a55038bd55748495d44530ecde
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2025-12-16 23:01:38 +0100

    basic styling for the sidebar

--- src/components/AppSideBar.vue
+++ src/components/AppSideBar.vue
@@ -1,16 +1,34 @@
 <script setup>
+import { Icon } from '@iconify/vue';
 </script>
 
 <template>
   <div class="side-bar">
-  hello I'm in the side bar
+    <Icon icon='solar:home-2-linear' class='icon-style clicked' />
+    <Icon icon='solar:sort-by-time-bold-duotone' class='icon-style' />
   </div>
 </template>
 
 <style scoped>
 .side-bar {
   height: 100vh;
-  width: 30px;
-  background-color: red;
+  width: 40px;
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  padding-top: 10px;
+  
+  .icon-style {
+    color: #ffffff;
+    width: 20px;
+    padding: 5px 4px;
+    border-radius: 5px;
+
+    &.clicked {
+      background-color: rgba(255,255,255, 0.3);
+    }
+  }
 }
 </style>
