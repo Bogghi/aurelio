1cc8a7b4ff6bf84d9b29b9b3a7cccc4ada32ab6a
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2025-12-16 01:00:14 +0100

    first drop of stuff

---
+++ src/components/AppSideBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="side-bar">
+  hello I'm in the side bar
+  </div>
+</template>
+
+<style scoped>
+.side-bar {
+  height: 100vh;
+  width: 30px;
+  background-color: red;
+}
+</style>
